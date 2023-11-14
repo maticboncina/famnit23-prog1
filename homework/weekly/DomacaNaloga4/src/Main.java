80e4708e55aac2e244b5af17ec319f31fe106d0d
Matic Bončina <github@express.ninja>
2023-11-14 11:00:11 +0100

    output fix

--- homework/weekly/DomacaNaloga4/src/Main.java
+++ homework/weekly/DomacaNaloga4/src/Main.java
@@ -4,6 +4,7 @@ public class Main{
     public static void main(String[] args){
         Scanner bralnik = new Scanner(System.in);
 
+        System.out.println("java DN3");
         System.out.print("Vpisi ime: ");
         String ime = bralnik.nextLine();
 
