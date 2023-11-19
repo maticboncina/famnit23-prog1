7a8ebc8692a7e351ea198445af0bf98f24efdbd6
Matic Bončina <github@express.ninja>
2023-11-19 10:32:45 +0100

    prvi kviz solution

---
+++ homework/quiz/PrviKviz/src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+        byte bit = Byte.MAX_VALUE;
+        System.out.println(bit);
+        System.out.println("");
+        /* 1 bajt */
+
+        short kratek = Short.MAX_VALUE;
+        System.out.println(kratek);
+        System.out.println("");
+        /* 2 bajta  */
+
+        int stevilka = Integer.MAX_VALUE;
+        System.out.println(stevilka);
+        System.out.println("");
+        /* 4 bajte */
+
+        long dolg = Long.MAX_VALUE;
+        System.out.println(dolg);
+        System.out.println("");
+        /* 8 bajtov */
+
+        float plavalec = Float.MAX_VALUE;
+        System.out.println(plavalec);
+        System.out.println("");
+        /* 4 bajte */
+
+        double dvojnik = Double.MAX_VALUE;
+        System.out.println(dvojnik);
+        System.out.println("");
+        /* 8 bajtov */
+
+        boolean logicarDa = true;
+        boolean logicarNe = false;
+        System.out.println(logicarDa);
+        System.out.println(logicarNe);
+        System.out.println("");
+        /* 1 **bit** */
+
+        char crka = 'A';
+        System.out.println(crka);
+        System.out.println("");
+        /* 2 bajta */
+    }
+}
