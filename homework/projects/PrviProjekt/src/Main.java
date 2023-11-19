eb1cf215b581bb564e65cc371009cbdbb225cef6
Matic Bončina <github@express.ninja>
2023-11-19 10:11:53 +0100

    first project initial commit

---
+++ homework/projects/PrviProjekt/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner bralnik = new Scanner(System.in);
+
+        System.out.println("Vnesi koliko stevil boš vnesel");
+        int steviloStevil = bralnik.nextInt();
+
+        int stevec = 1;
+        System.out.println("Vnesi stevilo #"+stevec);
+
+
+
+
+        
+
+        /**
+         * Kaj rabim narest tuki
+         * 1. Definiraj bralnik za koliko stevil
+         * 1.1 Naredi edge case, če mona notri vrže karkoli kar ni število
+         * 2. Naredi funkcijo, ki naredi array toliko velik, kot je bilo vpisano število
+         * 2.1 Naredi for loop ki bo bral kaj uporabnik vnesel in ga poknil v array vsakič + 1 mesto naprej do konca
+         * 3. funkcija, ki izpiše število elementov v tem arrayu
+         * 4. funckija, ki izpiše število različnih števil
+         * 5. funkcija, ki izpiše število sodih števil
+         * 6. funkcija, ki izpiše število lihih števil
+         * 7. funkcija, ki izpiše frekvenco pojavitev vsakega števila (v procentih)
+         * 8. funkcija, ki izpiše število, ki se največkrat ponovi
+         * 9. funkcija, ki izpiše največje število
+         * 10. funkcija, ki izpiše drugo najmanjšo vrednost števil
+         * 11. funkcija, ki izpiše povprečje vseh števil
+         * 12. funkcija, ki izpiše standardno deviacijo
+         * 13. funkcija, ki izpiše mediano
+         * 14. funkcija, ki izpiše vsoto vseh števil
+         * 15. funkcija, ki izpiše koliko je palindromnih števil
+         * 16. funkcija, ki izpiše največje palindromno število, ki je manjše od največjega števila v polju in hkrati palindrom (število ni nujno element polja)
+         * 17. Program naj na koncu še izpiše vsa števila, ki so v polju, ločena z vejicami, v obratnem vrstnem redu kot so bila vnešena.
+         */
+
+        /**
+         * Good to do:
+         * Funkcijo, ki se sprehaja skozi array?
+         * Funckije naj kličejo ena druga...
+         * Kakšen funkcije za basic job, kot je sesštevek vseh števil v arrayu etc...
+         */
+
+    }
+}
